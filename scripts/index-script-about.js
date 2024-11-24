d5176d9d4cb6bc9bf5a46d890511a44d92d3538c
Wanderer <a.vorontsovvv9@gmail.com>
2024-11-24 11:39:59 +0500

    Adding an "About Me" section
    Adding an "About Me" section

---
+++ scripts/index-script-about.js
@@ -0,0 +1,24 @@
+function switchVisible() {
+    if (document.getElementById("abouter").innerHTML.includes('About me')) {
+        document.getElementById("Div1").style.opacity  = 0;
+        document.getElementById("footer").style.opacity  = 0;
+        document.getElementById("Div1").style.pointerEvents = "none";
+        document.getElementById("footer").style.pointerEvents = "none";
+        document.getElementById("Div1").style.userSelect = "none";
+        document.getElementById("footer").style.userSelect = "none";
+        document.getElementById("abouter").textContent="< Return";
+        document.querySelector(".topography").style.opacity = 0.17;
+    } else {
+
+        document.getElementById("Div1").style.opacity  = 1;
+        document.getElementById("footer").style.opacity  = 1;
+        document.getElementById("Div1").style.pointerEvents = "all";
+        document.getElementById("footer").style.pointerEvents = "all";
+        document.getElementById("Div1").style.userSelect = "all";
+        document.getElementById("footer").style.userSelect = "all";
+        document.getElementById("abouter").textContent="About me";
+        document.querySelector(".topography").style.opacity = 0.3
+    }
+
+
+}
